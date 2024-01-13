ebaf67bf269d4de121caf4eca714c5c970d688e5
Atlanswer <atlanswer@gmail.com>
2024-01-14 01:01:53 +0800

    change fileformats option to unix

--- init.lua
+++ init.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.opt.fileformats = "unix"
+vim.opt.fileformats = "unix,dos"
 vim.opt.swapfile = false
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
