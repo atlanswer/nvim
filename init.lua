bd20ff446a44d66278a69eda64d6662067630afe
atlanswer <i@atlanswer.com>
2024-03-04 13:45:58 +0800

    add indent blankline

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ vim.opt.smartcase = true
 vim.opt.signcolumn = "yes"
 
 vim.opt.updatetime = 500
-vim.opt.timeoutlen = 500
+vim.opt.timeoutlen = 300
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
@@ -59,23 +59,23 @@ require("lazy").setup("plugins", {defaults = {lazy = true}})
 -- keymap for leap
 local function create_leap_mappings()
     for _, _1_ in ipairs({{{"n", "x", "o"}, "s", "<Plug>(leap-forward)", "Leap forward"}, {{"n", "o"}, "S", "<Plug>(leap-backward)", "Leap backward"}, {{"n", "x", "o"}, "gs", "<Plug>(leap-from-window)", "Leap from window"}}) do
-	local _each_2_ = _1_
-	local modes = _each_2_[1]
-	local lhs = _each_2_[2]
-	local rhs = _each_2_[3]
-	local desc = _each_2_[4]
-	for _0, mode in ipairs(modes) do
-	    local rhs_2a = vim.fn.mapcheck(lhs, mode)
-	    if (rhs_2a == "") then
-		vim.keymap.set(mode, lhs, rhs, {silent = true, desc = desc})
-	    else
-		if (rhs_2a ~= rhs) then
-		    local msg = ("leap.nvim: create_default_mappings() " .. "found conflicting mapping for " .. lhs .. ": " .. rhs_2a)
-		    vim.notify(msg, vim.log.levels.WARN)
-		else
-		end
+      local _each_2_ = _1_
+      local modes = _each_2_[1]
+      local lhs = _each_2_[2]
+      local rhs = _each_2_[3]
+      local desc = _each_2_[4]
+      for _0, mode in ipairs(modes) do
+        local rhs_2a = vim.fn.mapcheck(lhs, mode)
+          if (rhs_2a == "") then
+            vim.keymap.set(mode, lhs, rhs, {silent = true, desc = desc})
+          else
+            if (rhs_2a ~= rhs) then
+              local msg = ("leap.nvim: create_default_mappings() " .. "found conflicting mapping for " .. lhs .. ": " .. rhs_2a)
+              vim.notify(msg, vim.log.levels.WARN)
+            else
             end
-        end
+          end
+      end
     end
     return nil
 end
