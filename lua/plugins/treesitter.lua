5643dc7c625c908d91d443c4faf21e688c522b2b
Atlanswer <atlanswer@gmail.com>
2024-07-29 18:39:42 +0800

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -17,11 +17,5 @@ return {
         },
         indent = { enable = true, disable = { 'ruby' } },
     },
-    config = function(_, opts)
-        -- Prefer git instead of curl in order to improve connectivity in some environments
-        require('nvim-treesitter.install').prefer_git = true
-        ---@diagnostic disable-next-line: missing-fields
-        require('nvim-treesitter.configs').setup(opts)
-        end,
     cond = not vim.g.vscode,
 }
