7df875f3e2bc5b1737987a33b8f1680e02b5c422
Atlanswer <atlanswer@gmail.com>
2024-09-12 20:52:21 +0800

    update dependencies

--- init.lua
+++ init.lua
@@ -53,7 +53,7 @@ vim.opt.scrolloff = 10
 
 -- Load Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
     local out = vim.fn.system({
         "git",
