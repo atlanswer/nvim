0feff0399d96a0e143ae82958121ecc535bae925
atlanswer <i@atlanswer.com>
2024-12-26 17:43:51 +0800

    lazy load typescript tools

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -1,6 +1,11 @@
 return {
     "pmizio/typescript-tools.nvim",
+    event = "VeryLazy",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
+    opts = {
+        settings = {
+            expose_as_code_action = "all",
+        },
+    },
     cond = not vim.g.vscode,
 }
