aa6b909b78bfe590b549b3918e2c1af5959ef64f
Atlanswer <atlanswer@gmail.com>
2025-05-14 20:46:16 +0800

    too many text objects

--- lua/plugins/treesitter-textobjects.lua
+++ lua/plugins/treesitter-textobjects.lua
@@ -53,7 +53,7 @@ return {
                         },
                         --
                         -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
-                        ["]o"] = "@loop.*",
+                        -- ["]o"] = "@loop.*",
                         -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
                         --
                         -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
@@ -84,12 +84,12 @@ return {
                     -- Below will go to either the start or the end, whichever is closer.
                     -- Use if you want more granular movements
                     -- Make it even more gradual by adding multiple queries and regex.
-                    goto_next = {
-                        ["]d"] = "@conditional.outer",
-                    },
-                    goto_previous = {
-                        ["[d"] = "@conditional.outer",
-                    },
+                    -- goto_next = {
+                    --     ["]d"] = "@conditional.outer",
+                    -- },
+                    -- goto_previous = {
+                    --     ["[d"] = "@conditional.outer",
+                    -- },
                 },
                 lsp_interop = {
                     enable = true,
