ab81ab9a0d7dfc34bfdff269e19010c8b40a6ba8
Atlanswer <i@atlanswer.com>
2026-01-25 01:21:29 +0800

    chore: remove lazy check update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -37,7 +37,5 @@ require("lazy").setup {
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
     install = { colorscheme = { "rose-pine" } },
-    -- automatically check for plugin updates
-    checker = { enabled = true, frequency = 60 * 60 * 12 },
     rocks = { enabled = false },
 }
